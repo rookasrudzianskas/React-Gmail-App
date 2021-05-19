ccdd712a279d442604d0e42fa492f4cc5819be24
Rokas <rookas.rudzenskas@gmail.com>
2021-05-19 22:05:36 +0300

    Rokas

--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -1,5 +1,4 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counterAPI';
 
 const initialState = {
   value: 0,
@@ -14,9 +13,7 @@ const initialState = {
 export const incrementAsync = createAsyncThunk(
   'counter/fetchCount',
   async (amount) => {
-    const response = await fetchCount(amount);
     // The value we return becomes the `fulfilled` action payload
-    return response.data;
   }
 );
 
