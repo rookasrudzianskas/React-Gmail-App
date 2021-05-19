29b657e8f78cfddea5501027b1ddb0ad264515ec
Rokas <rookas.rudzenskas@gmail.com>
2021-05-19 22:14:50 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <div className="app">
-     <h2>Hello 🚀</h2>
+        <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./styles/Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Header</h1>
+        </div>
+    );
+};
+
+export default Header;
